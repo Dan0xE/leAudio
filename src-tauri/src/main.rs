47751b558cdeed6f4a7536a54fa3d3f3cdfcea0d
Dan0xE <danopperm@gmail.com>
2023-04-22 04:36:52 +0200

    removed check_tauri_window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn check_tauri_window() -> bool {
-    true
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![check_tauri_window])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
